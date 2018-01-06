bc40b6b84dbff18feb806ad6741e9a8927c2a124
tunc bahcecioglu <tunc.bahcecioglu@gmail.com>
2018-01-07 00:13:43 +0100

    Issue 1
    added example blitz code to the testing project

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
-#include "hello.h"
+#include <blitz/array.h>
 
-int main() {
-    hello();
+using namespace blitz;
+
+int main()
+{
+    Array<float,2> A(3,3), B(3,3), C(3,3);
+
+    A = 1, 0, 0,
+        2, 2, 2,
+        1, 0, 0;
+
+    B = 0, 0, 7,
+        0, 8, 0,
+        9, 9, 9;
+
+    C = A + B;
+
+    cout << "A = " << A << endl
+         << "B = " << B << endl
+         << "C = " << C << endl;
+
+    return 0;
 }
