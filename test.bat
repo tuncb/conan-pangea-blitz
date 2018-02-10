80b9752cecceeb94e8d4b27428224e7c65f58a61
tunc bahcecioglu <tunc.bahcecioglu@gmail.com>
2018-02-10 18:20:41 +0100

    fix #6 add debug binary support
    * add debug and release binaries into single package
    * add testing for the debug build
    * remove test comments from test_package/CMakeLists.txt

--- test.bat
+++ test.bat
@@ -1,2 +1,3 @@
 conan export-pkg . blitz/1.0.1@tuncb/pangea -s os=Windows -s compiler="Visual Studio" -s arch="x86_64" -s compiler.toolset=v141 --force
-conan test ./test_package blitz/1.0.1@tuncb/pangea -s os=Windows -s compiler="Visual Studio" -s arch="x86_64" -s compiler.toolset=v141
+conan test ./test_package blitz/1.0.1@tuncb/pangea -s build_type=Debug -s os=Windows -s compiler="Visual Studio" -s arch="x86_64" -s compiler.toolset=v141
+conan test ./test_package blitz/1.0.1@tuncb/pangea -s build_type=Release -s os=Windows -s compiler="Visual Studio" -s arch="x86_64" -s compiler.toolset=v141
